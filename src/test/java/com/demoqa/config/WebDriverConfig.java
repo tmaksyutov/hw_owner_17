60d03c3de911e3036631dcad8b5040a3836e4d63
TMaksyutov <dadojkeee@mail.ru>
2022-11-27 20:16:22 +0300

    added config

---
+++ src/test/java/com/demoqa/config/WebDriverConfig.java
@@ -0,0 +1,29 @@
+package com.demoqa.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:${host}.properties"
+}
+)
+public interface WebDriverConfig extends Config {
+
+    @Key("baseUrl")
+    @DefaultValue("https://demoqa.com")
+    String baseUrl();
+
+    @Key("browser")
+    @DefaultValue("chrome")
+    String browser();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String browserSize();
+
+    @Key("browserVersion")
+    String browserVersion();
+
+    @Key("remoteUrl")
+    String remoteUrl();
+}
+
---
+++ src/test/java/com/demoqa/config/WebDriverProvider.java
@@ -0,0 +1,26 @@
+package com.demoqa.config;
+
+import com.codeborne.selenide.Configuration;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class WebDriverProvider {
+    public static WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+
+    public static void configure() {
+
+            Configuration.baseUrl = config.baseUrl();
+            Configuration.browser = config.browser();
+            Configuration.browserSize = config.browserSize();
+            Configuration.browserVersion = config.browserVersion();
+            System.setProperty("chromeoptions.prefs","intl.accept_languages=ru");
+            String configSource = config.remoteUrl();
+            if (configSource != null) {
+                Configuration.remote = config.remoteUrl();
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                Configuration.browserCapabilities = capabilities;
+                capabilities.setCapability("enableVNC", true);
+                capabilities.setCapability("enableVideo", true);
+            }
+        }
+}
